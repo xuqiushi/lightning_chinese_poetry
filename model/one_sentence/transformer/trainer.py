bf69e09b2d13f201ae6d9d919ff64c938931e53c
qiushi.xu <yingxiongxqs@126.com>
2022-04-15 14:20:35 +0800

    transformer版本

--- model/one_sentence/transformer/trainer.py
+++ model/one_sentence/transformer/trainer.py
@@ -44,10 +44,10 @@ class Trainer:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.data_loader = OneSentenceLoader(
             directory=self.data_directory,
-            train_n_workers=12,
+            train_n_workers=8,
             train_batch_size=BATCH_SIZE,
             train_pre_fetch_factor=1,
-            val_n_workers=6,
+            val_n_workers=8,
             val_batch_size=BATCH_SIZE,
             val_pre_fetch_factor=1,
             device=self.device,
