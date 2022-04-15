4cc8b31b7626c6193b496f039336703c61543f0e
qiushi.xu <yingxiongxqs@126.com>
2022-04-15 17:20:19 +0800

    transformer版本

--- model/one_sentence/transformer/trainer.py
+++ model/one_sentence/transformer/trainer.py
@@ -47,7 +47,7 @@ class Trainer:
             directory=self.data_directory,
             train_n_workers=4,
             train_batch_size=BATCH_SIZE,
-            train_pre_fetch_factor=32,
+            train_pre_fetch_factor=64,
             val_n_workers=4,
             val_batch_size=BATCH_SIZE,
             val_pre_fetch_factor=8,
