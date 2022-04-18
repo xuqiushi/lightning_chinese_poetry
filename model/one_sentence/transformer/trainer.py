f6c90422f80a7d33ee548812b0e953cabd6a91e9
qiushi.xu <yingxiongxqs@126.com>
2022-04-18 10:00:52 +0800

    transformer版本

--- model/one_sentence/transformer/trainer.py
+++ model/one_sentence/transformer/trainer.py
@@ -108,9 +108,9 @@ class Trainer:
             self.device,
             STR_MAX_LENGTH
         )
-        self.model = torch.jit.script(Seq2Seq(
+        self.model = Seq2Seq(
             enc, dec, self.src_pad_idx, self.trg_pad_idx, self.device
-        ).to(self.device))
+        ).to(self.device)
         self.count_parameters(self.model)
         self.model.apply(self.initialize_weights)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
