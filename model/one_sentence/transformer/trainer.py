b26d885549fc43b7b39e1f73871660e859e7c226
qiushi.xu <yingxiongxqs@126.com>
2022-04-21 16:56:28 +0800

    中间保存

--- model/one_sentence/transformer/trainer.py
+++ model/one_sentence/transformer/trainer.py
@@ -4,7 +4,7 @@ from etl.dataset.seq2seq_data_loader import Seq2seqDataLoader
 from etl.entity.seq2seq.data_loader_parameter import DataLoaderParameter
 from etl.entity.seq2seq.data_transformer_parameter import DataTransformerParameter
 from etl.etl_contants import TANG_SONG_SHI_DIRECTORY
-from etl.title_first.title_first_transformer import TitleFirstTransformer
+from etl.one_sentence_arrow.raw_data_transformer import RawDataTransformer
 from model.base.seq2seq_trainer import Seq2seqTrainer
 from model.entity.train_parameter import TrainParameter
 from model.entity.transformer_model_parameter import TransformerModelParameter
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     torch.autograd.profiler.emit_nvtx(False)
     # torch.backends.cudnn.benchmark = True
 
-    data_transformer = TitleFirstTransformer(
+    data_transformer = RawDataTransformer(
         seq2seq_data_transformer_parameter=DATA_TRANSFORMER_PARAMETER
     )
     data_loader = Seq2seqDataLoader(
